167a12221610aacf6c047b48687bf3e3a792a27d
MenSeb <sebastien.menard.github@gmail.com>
2019-04-10 13:05:51 -0400

    update App structure to test inputs

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import * as INPUTS from '../constants'
 import { CalculatorProvider } from '../contexts/calculator'
 import { Equation, Inputs, Message, Result } from '../components'
 
 export default function App ()
 {
+  const { current: CalculatorInputs } = useRef(
+    Object.entries( INPUTS ).map(
+      ([ name, inputs ]) => (
+        <Inputs
+          key={ name }
+          inputs={ inputs }
+          className={ name }
+        />
+      )
+    )
+  )
+
   return (
     <div className='App'>
       <CalculatorProvider>
@@ -14,7 +27,7 @@ export default function App ()
 
         <Message />
 
-        <Inputs />
+        { CalculatorInputs }
 
       </CalculatorProvider>
     </div>
