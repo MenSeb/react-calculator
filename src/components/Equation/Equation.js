23a1ad13603792254f02daf467ea70747a5ca3f8
MenSeb <sebastien.menard.github@gmail.com>
2019-04-09 21:31:47 -0400

    equation rendering with context

--- src/components/Equation/Equation.js
+++ src/components/Equation/Equation.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CalculatorContext } from '../../contexts/calculator'
 
 export default function Equation ()
 {
+  const { equation } = useContext( CalculatorContext )
+
   return (
     <output className='Equation'>
+      { equation }
     </output>
   )
 }
