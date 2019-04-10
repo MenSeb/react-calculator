32cd01f5b23b850fc682cb6a3f83fcd3632c4b9c
MenSeb <sebastien.menard.github@gmail.com>
2019-04-10 15:37:13 -0400

    update application structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,18 @@
-import React, { useRef } from 'react'
-import * as INPUTS from '../constants'
+import React from 'react'
 import { CalculatorProvider } from '../contexts/calculator'
-import { Equation, Inputs, Message, Result } from '../components'
+import { Operands, Operators, Result } from '../components'
 
 export default function App ()
 {
-  const { current: CalculatorInputs } = useRef(
-    Object.entries( INPUTS ).map(
-      ([ name, inputs ]) => (
-        <Inputs
-          key={ name }
-          inputs={ inputs }
-          className={ name }
-        />
-      )
-    )
-  )
-
   return (
     <div className='App'>
       <CalculatorProvider>
 
-        <Equation />
-
         <Result />
 
-        <Message />
+        <Operands />
 
-        { CalculatorInputs }
+        <Operators />
 
       </CalculatorProvider>
     </div>
