13b3c3a29000e7e3bdefea659f9f8e62e13cfd56
MenSeb <sebastien.menard.github@gmail.com>
2019-04-10 13:13:36 -0400

    update component name and className

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { CalculatorContext } from '../../contexts/calculator';
 
-export default function Output ()
+export default function Result ()
 {
   const { result } = useContext( CalculatorContext )
 
   return (
-    <output className='Output'>
+    <output className='Result'>
       { result }
     </output>
   )
