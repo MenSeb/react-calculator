6ac063438c0005b30ad6048b043eaeede341a343
MenSeb <sebastien.menard.github@gmail.com>
2019-04-09 21:33:06 -0400

    message rendering with context

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CalculatorContext } from '../../contexts/calculator';
 
 export default function Message ()
 {
+  const { message } = useContext( CalculatorContext )
+
   return (
     <output className='Message'>
+      { message }
     </output>
   )
 }
