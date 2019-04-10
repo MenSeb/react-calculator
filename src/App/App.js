e5226c2426daee7478273c932c3d435748e676f6
MenSeb <sebastien.menard.github@gmail.com>
2019-04-09 20:29:47 -0400

    application structure

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,6 @@ export default function App ()
 {
   return (
     <div className='App'>
-
       <CalculatorProvider>
 
         <Equation />
@@ -18,7 +17,6 @@ export default function App ()
         <Inputs />
 
       </CalculatorProvider>
-
     </div>
   )
 }
