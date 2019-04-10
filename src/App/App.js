d9c34ba40fc69927fe5ffe2681a2ed12ef9a61be
MenSeb <sebastien.menard.github@gmail.com>
2019-04-09 20:28:56 -0400

    application structuer

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { CalculatorProvider } from '../contexts'
+import { Equation, Output, Message, Inputs } from '../components'
 
 export default function App ()
 {
   return (
     <div className='App'>
+
+      <CalculatorProvider>
+
+        <Equation />
+
+        <Output />
+
+        <Message />
+
+        <Inputs />
+
+      </CalculatorProvider>
+
     </div>
   )
 }
