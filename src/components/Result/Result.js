dd00bc6445afa03f80f3b60dc41c1e1b1150c0e7
MenSeb <sebastien.menard.github@gmail.com>
2019-04-09 21:33:49 -0400

    result rendering with context

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CalculatorContext } from '../../contexts/calculator';
 
 export default function Output ()
 {
+  const { result } = useContext( CalculatorContext )
+
   return (
     <output className='Output'>
-
+      { result }
     </output>
   )
 }
