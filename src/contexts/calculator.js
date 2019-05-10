4889f1a2c9df131327e4f867dcdd74eb43281867
MenSeb <sebastien.menard.github@gmail.com>
2019-05-09 21:50:27 -0400

    update calculator context

--- src/contexts/calculator.js
+++ src/contexts/calculator.js
@@ -1,20 +1,28 @@
-import React, { createContext } from 'react'
+import React, { createContext, useRef } from 'react'
 import { useCalculator } from '../hooks'
+import { HELPERS, MODES, OPERANDS, OPERATORS, SCIFIS, TRIGOS } from '../constants'
 
 const CalculatorContext = createContext()
 
 const { Consumer: CalculatorConsumer, Provider } = CalculatorContext
 
-function CalculatorProvider ({ children, ...props })
+function CalculatorProvider ( props )
 {
-  const { calculate, ...calculator } = useCalculator()
+  const { current: initial } = useRef({
+    HELPERS,
+    INPUTS: {
+      operand: OPERANDS,
+      operator: OPERATORS,
+      scifi: SCIFIS,
+      trigo: TRIGOS,
+    },
+    MODES,
+  })
+
+  const calculator = useCalculator()
 
   return (
-    <Provider value={ calculator }>
-      <form className='Calculator basic' onClick={ calculate }>
-        { children }
-      </form>
-    </Provider>
+    <Provider { ...props } value={{ ...initial, ...calculator }} />
   )
 }
 
