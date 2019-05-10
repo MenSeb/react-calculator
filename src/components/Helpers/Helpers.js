c1bee2a3113bd1189c3c3af14c1e6998b6472b1f
MenSeb <sebastien.menard.github@gmail.com>
2019-05-09 21:48:23 -0400

    update Helpers rendring with useMemo and context

--- src/components/Helpers/Helpers.js
+++ src/components/Helpers/Helpers.js
@@ -1,9 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { CalculatorContext } from '../../contexts/calculator';
 
 export default function CalculatorHelpers ()
 {
-  const { helpers } = useContext( CalculatorContext )
+  const { HELPERS } = useContext( CalculatorContext )
+
+  const helpers = useMemo(
+    () => Object.entries( HELPERS ).map(
+      ([ value, title ]) => (
+        <input
+          key={ value }
+          title={ title }
+          type='button'
+          value={ value }
+        />
+      )
+    ),
+    [ HELPERS ]
+  )
 
   return (
     <section id='CalculatorHelpers'>
